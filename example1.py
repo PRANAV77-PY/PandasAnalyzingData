f0a5b78869650e45eb8c954b3769768e2634e56b
PRANAV77-PY <pbse1377@gmail.com>
2025-09-20 17:08:18 +0530

    Add files via upload

---
+++ example1.py
@@ -0,0 +1,13 @@
+#head first 12 rows
+import pandas as pd
+
+df = pd.read_csv('D:\Software engg Professional training\Python Basics\Python Pandas\pandas Read CSV\data.csv')
+
+print(df.head(12))
+
+#tail bottom rows
+import pandas as pd
+
+df = pd.read_csv('D:\Software engg Professional training\Python Basics\Python Pandas\pandas Read CSV\data.csv')
+
+print(df.tail(11))
---
+++ example2.py
@@ -0,0 +1,6 @@
+#info about the data
+import pandas as pd
+
+df = pd.read_csv('D:\Software engg Professional training\Python Basics\Python Pandas\pandas Read CSV\data.csv')
+
+print(df.info())
